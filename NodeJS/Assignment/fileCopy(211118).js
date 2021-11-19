b8046082302dc271376c83c9be6674b227e26d5c
jhin93 <scene1993@gmail.com>
2021-11-19 16:09:04 +0900

    Buffer 과제 완료

--- NodeJS/Assignment/fileCopy(211118).js
+++ NodeJS/Assignment/fileCopy(211118).js
@@ -1,47 +1,51 @@
 const fs = require('fs');
-var buf = Buffer.alloc(2048000)
+var buf = Buffer.alloc(20480)
 
 var pos = 0; // 읽는 위치
 var remainSize = 0; // 붙여넣어질 파일의 사이즈
-// 2개의 파일을 열어야 한다. 복사할 파일, 붙여넣을 파일
+var srcFileName = ''; // 원본파일명
+var destFileName = ''; // 복사파일명
 
-// 복사할 파일 open - read로 읽기? stat?
-// 붙여넣을 파일 open - write로 붙여넣기
+// 원본 파일 읽어오기
+fs.readdir("./source", (err, files) => {
+    if (err) throw err; // 에러처리
+    console.log(files);
+    // forEach로 파일들에 차례대로 콜백함수를 적용
+    files.forEach((file) => {
+        if ("metallica_seattle.mp4" === file) { // 원본파일 일때를 조건으로 설정.
 
-// 1. 붙여넣을 파일 열기
-fs.open('copy.mp4', 'r', function(err, fd) {
-    // 에러처리
-    if(err) return console.error(err);
-    // 2. 비디오 파일 내용 fs.stat()으로 가져오기.
-    fs.stat('../nodeapp/metallica_seattle.mp4', (err, stat) => {
-        if(err) return console.error(err);
-        console.log('원본크기', stat.size);
-        remainSize = stat.size // 사이즈만 먼저 확보해놓고
-    })
-    
-    // 3. read 내에서 복사 붙여넣기 시작
-    // 파일 디스크럽터 / 데이터가 쓰여질 버퍼 / 데이터가 쓰여질 버퍼의 시작 위치 / 데이터를 읽을 문자열의 크기 /  데이터를 읽을 파일의 읽기 시작할 위치 / callback 함수
-    fs.read(fd, buf, 0, buf.length, pos, (err, bytes) => {
-        // 에러처리
-        if(err) return console.error(err);
+            srcFileName = "./source" + file; // 원본파일명 주소 설정
+            destFileName = "./target" + file; // 복사파일명 주소 설정. 다른 디렉토리니까 ../target아닌가 싶다. 문제가 생기면 여기부터 체크
+            fs.stat(srcFileName, (err, stats) => { // 파일의 정보를 읽기위해 fs.stat()사용
+                if(err) throw err; // 에러처리
 
-        post += 2048; // 읽을 위치를 2048씩 늘림
-        remainSize -= bytes; // 위치를 옮긴 만큼 줄어든다. 이해가 안되니까 콘솔에 찍어볼 것
-        
-        // 원본 사이즈가 버퍼보다 클때. 아직 더 들어가야 함.
-        if(remainSize > buf.length) {
-            //읽고
-            //쓰고
+                remainSize = stats.size // 변수에 원본파일 사이즈 할당
 
-        // 이젠 원본 사이즈가 버퍼보다 작음. 남은거 넣기
-        } else {
-            // while 문 사용
-            //읽고
-            //쓰고
-            // write file close
-            // break;
+                var rfd = fs.openSync(srcFileName, 'r'); // 읽기전용(r)으로 원본파일 열기
+                var wfd = fs.openSync(destFileName, 'a'); // 추가 쓰기(a)로 열기. 파일이 존재하지 않으면 만들어짐. 복사파일 열기
+
+                // 원본 데이터를 담을 변수 선언.
+                var readBytes = 0;
+                while(true) { // 본격적으로 복사파일에 원본 데이터를 넣기 위해 while 문 사용.
+                    // fs.read(fd, buffer, offset, length, position, callback)
+                    // 파일을 읽을 때 buffer 라는 임시공간에 적어놓고 읽는다. fd: 읽을 대상 / buffer: 읽기위해 읽을 것을 적는 공간 / offset: 버퍼에 데이터가 쓰여지기 시작하는 위치 / length: 버퍼를 읽을 때 length만큼씩 읽는다 / position: 데이터를 버퍼에 다 적었으면 position부터 읽는다 / callback : 이후 데이터를 다룰 콜백함수.
+                    readBytes = fs.readSync(rfd, buf, 0, buf.length, pos) // rfd 파일 디스크립터에서 읽어들인 것을 buf에 임시로 작성한다. 시작위치는 0부터 작성하고 buf.length만큼씩 임시로 적어넣는다. 다 적었으면 pos부터 읽는다.
+                    if(remainSize > buf.length){ // 읽어야 될 원본파일의 크기가 남은 버퍼 크기보다 클때. 즉, 막 읽기 시작해서 버퍼가 점점 차오르는 과정.
+                        fs.writeFileSync(wfd, buf, 0, buf.length); // buffer를 fd에 의해 특정된 파일에 작성한다. wfd에 buffer를 적는다. 시작부터 적고(offset이 0), buf.length만큼씩 작성해넣는다.
+                        pos += readBytes; // 작성했으니까 읽기 시작하는 position을 증가시킨다. 버퍼 크기만큼(20480) 증가할 것.
+                        remainSize -= readBytes; // 읽을 때마다 원본 파일 사이즈에서 읽은 만큼씩 뺀다.
+                    } else { // 마지막 1회에 다다랐을 때
+                        fs.writeSync(wfd, buf, 0, readBytes) // 남은 readBytes 만큼 작성한다.
+                        fs.closeSync(rfd); // 오픈한 것을 닫는다.
+                        fs.closeSync(wfd); // 오픈한 것을 닫는다.
+                        break; // while문 마무리
+                    }
+                }
+            })
+
+            return false; // if ("metallica_seattle.mp4" === file)의 로직을 만족시켰으면 빠져나가야됨.
         }
-
-        if(bytes > 0) console.log(buf.slice(0, bytes).toString())
     })
 })
+
+
