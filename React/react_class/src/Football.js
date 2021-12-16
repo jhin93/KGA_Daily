7e7e15e6eda80ac1952538a2b766474293ef0a6d
jhin93 <scene1993@gmail.com>
2021-12-16 10:48:42 +0900

    event 예약어로 이벤트를 표현

--- React/react_class/src/Football.js
+++ React/react_class/src/Football.js
@@ -1,10 +1,21 @@
-function Football() {
-    const shoot = (arg) => {
-        alert(arg);
-    }
+// function Football() {
+//     const shoot = (arg) => {
+//         alert(arg);
+//     }
+
+//     return (
+//         <button onClick={() => shoot("The tremendous Goal Son!!!")}>Take the shot!</button>
+//     );
+// }
+
+function Football () {
+    const shoot = (a, e) => {
+        alert(a + ", " + e.type);
+    };
 
     return (
-        <button onClick={() => shoot("The tremendous Goal Son!!!")}>Take the shot!</button>
+        // event는 트리거된 이벤트를 실제로 담아낸다. 그래서 얼럿에 onMouseover가 찍힘.
+        <button type="button" onMouseOver={(event) => shoot("The tremendous Goal Son!!!", event)}>Take the shot !!</button>
     );
 }
 
