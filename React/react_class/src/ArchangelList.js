18775867c4f1dd618949a5c03b955c0c6627b99a
jhin93 <scene1993@gmail.com>
2021-12-16 12:42:47 +0900

    map으로 list 찍어내기 2

--- React/react_class/src/ArchangelList.js
+++ React/react_class/src/ArchangelList.js
@@ -3,22 +3,39 @@ function Gundam(props) {
 };
 
 
-function ArchangelList() {
-    const gundamList = [
-        "ZMGF-X10A Freedom Gundam", 
-        "ZMGF-X109A Justice Gundam", 
-        "Strike Gundam"
-    ]
+// function ArchangelList() {
+//     const gundamList = [
+//         "ZMGF-X10A Freedom Gundam", 
+//         "ZMGF-X109A Justice Gundam", 
+//         "Strike Gundam"
+//     ]
+
+//     return (
+//         <>
+//             <h1>Who lives in Archangel???</h1>
+//             <ul>
+//                 {/* map 메소드로 li를 찍어냄 */}
+//                 {gundamList.map((robot) => <Gundam model={robot} />)}
+//             </ul>
+//         </>
+//     );
+// };
+
+function Archangel() {
+    const gundams = [
+        {id: 1, model: "ZMGF-X10A Freedom Gundam"},
+        {id: 2, model: "ZMGF-X109A Justice Gundam"},
+        {id: 3, model: "Sword Strike Gundam"}
+    ];
 
     return (
         <>
-            <h1>Who lives in Archangel???</h1>
+            <h1>Who lives in Archangel?</h1>
             <ul>
-                {/* map 메소드로 li를 찍어냄 */}
-                {gundamList.map((robot) => <Gundam model={robot} />)}
+                {gundams.map((mobileSuite) => <Gundam key={mobileSuite.id} model={mobileSuite.model}/>)}
             </ul>
         </>
-    );
-};
+    )
+}
 
-export default ArchangelList;
+export default Archangel;
