fa763d7040bcf85090bba408cf4563e31366e3ae
jhin93 <scene1993@gmail.com>
2021-12-17 13:05:49 +0900

    setState 1. 상태를 직접 조작 2. 상태를 조작하는 함수로 인자를 받음

--- React/react_class/src/Counter.js
+++ React/react_class/src/Counter.js
@@ -4,14 +4,14 @@ import React from "react";
 function Counter() {
     const [number, setNumber] = useState(0);
     const onIncrease = () => {
-        setNumber(number + 1)
-        // setNumber(prevNumber => prevNumber + 1) // 이전 상태
+        setNumber(number + 1) // 상태를 직접 조작
+        // setNumber(asd => asd + 1) // 상태를 업데이트하는 함수를 넣어줌
         console.log(number) // number를 찍으니까 이전 state가 나온다
     }
 
     const onDecrease = () => {
         setNumber(number - 1)
-        // setNumber(prevNumber => prevNumber - 1) // 이전 상태
+        // setNumber(asd => asd - 1) // 상태를 업데이트하는 함수를 넣어줌
         console.log(number) // number를 찍으니까 이전 state가 나온다
     }
 
