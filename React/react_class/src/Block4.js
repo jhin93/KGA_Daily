777ea88fcce780e57ac4aee793ef082ad88327af
jhin93 <scene1993@gmail.com>
2021-12-20 12:52:06 +0900

    글 안나옴

--- React/react_class/src/Block4.js
+++ React/react_class/src/Block4.js
@@ -1,20 +1,17 @@
 import React from "react";
-import {Route , BrowserRouter as Router, Routes} from "react-router-dom";
-// import Layout from "./pages/Layout";
-import About from "./pages/About";
-// import NoPage from "./pages/Nopage";
-// import Contact from "./pages/Contact";
+import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 export default function Block4() {
     return (
-        <div>
-            <Router>
-                <Routes>
-                    <Route path="/" component={Home} /> {/* 임포트한 컴포넌트를 직접 넣을때는 component라고 쓴다. <> 안에 넣으려면 element라고 해야 됨*/}
-                    <Route path="/about" component={About} /> 
-                </Routes>
-            </Router>
-        </div>        
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     )
-}
+  }
