4127e3fbbcb7df1cce7843734b133074f39bc263
jhin93 <scene1993@gmail.com>
2021-12-20 10:26:25 +0900

    빈태그 대신 React.Fragment 입력도 가능

--- React/react_class/src/Jsxtest.js
+++ React/react_class/src/Jsxtest.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function Jsxtest() {
     return(
-        <>
+        <React.Fragment>
             <div>
                 <h1>Hello world!!!</h1>
             </div>
             <div>
                 <h1>Hello world!!!</h1>
             </div>
-        </>
+        </React.Fragment>
     );
 }
 
