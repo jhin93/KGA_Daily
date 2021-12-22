70936f7e274f66bdcc6f513c0f6550bf229e12db
jhin93 <scene1993@gmail.com>
2021-12-22 14:50:02 +0900

    checkbox 완료

--- React/react_class2/src/CheckBox.js
+++ React/react_class2/src/CheckBox.js
@@ -4,6 +4,7 @@ function CheckBox({children, checked, ...rest}) {
     return (
         <div>
             <label>
+                {/* 체크가 되면 cheked 속성이 true가 됨(checked=true로 됨. 즉 {checked}가 true가 되는 것)  */}
                 <input type="checkbox" checked={checked} {...rest} />
             </label>
             <span>{children}</span>
--- React/react_class2/src/Reactcss.js
+++ React/react_class2/src/Reactcss.js
@@ -4,7 +4,8 @@ import CheckBox from "./CheckBox";
 function Reactcss () {
     const [check, setCheck] = useState(false)
     const onChange = (e) => {
-        setCheck(e.target.checked) // 체크 이벤트드를 받는 것.
+        // CheckBox.js에서 전달받은 checked 속성의 boolean 타입을 check에 세터함수로 넣어줌. 그 결과가 렌더링되는 것.
+        setCheck(e.target.checked) // 체크 이벤트를 받는 것.
     };
 
     return (
