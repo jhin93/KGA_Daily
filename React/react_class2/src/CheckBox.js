5a8c3e7c213fbe84a01171e5e14979234da4cb7e
jhin93 <scene1993@gmail.com>
2021-12-22 15:36:19 +0900

    css 완료

--- React/react_class2/src/CheckBox.js
+++ React/react_class2/src/CheckBox.js
@@ -1,11 +1,21 @@
 import React from "react"
+import {MdCheckBox, MdCheckBoxOutlineBlank} from "react-icons/md";
+import Styles from "./CheckBox.module.css"
 
 function CheckBox({children, checked, ...rest}) {
     return (
-        <div>
+        <div className={Styles.checkbox}>
             <label>
                 {/* 체크가 되면 cheked 속성이 true가 됨(checked=true로 됨. 즉 {checked}가 true가 되는 것)  */}
                 <input type="checkbox" checked={checked} {...rest} />
+                {/* 체크가 됐으면 MdCheckBox 아니면 MdCheckBoxOutlineBlank*/}
+                <div className={Styles.icon}>
+                    {checked ? (
+                        <MdCheckBox className={Styles.checked}/>
+                    ) : (
+                        <MdCheckBoxOutlineBlank />
+                    )}
+                </div> 
             </label>
             <span>{children}</span>
         </div>
