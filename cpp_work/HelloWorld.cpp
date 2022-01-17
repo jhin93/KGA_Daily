3c566a479d37d8affd5e771776edeed79929e6ac
jhin93 <scene1993@gmail.com>
2022-01-17 13:42:04 +0900

    1/17 C++ 첫 실행

---
+++ cpp_work/HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!!" << std::endl;
+
+    return 0;
+    // HelloWorld를 실행한다. os가 받아준다. start-up 코드가 main을 받는 것.
+    // return 0; 은 exit code이다. os에게 알려준다.
+}
