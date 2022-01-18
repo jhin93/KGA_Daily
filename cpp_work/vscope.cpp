66d5ae448409be863b7b0100825ee6625de80dc6
jhin93 <scene1993@gmail.com>
2022-01-18 12:52:19 +0900

    지역 및 전역 변수에 대해 이름이 같을 수 있다. 함수 내부의 지역변수 값이 우선한다.

--- cpp_work/vscope.cpp
+++ cpp_work/vscope.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 
+int gVar; // 전역변수는 모든 함수 외부에 선언
+
 int main()
 {
     int a, b;
     int c;
+    // int gVar;
 
     a = 10;
     b = 40;
     c = a + b;
+    // gVar = a + b;
 
     std::cout << "c = a + b : " << c << std::endl;
+    std::cout << "gVar = a + b : " << gVar << std::endl;
 
     return 0;
 }
