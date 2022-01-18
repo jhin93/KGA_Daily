e8db5db12e06e81e6833701c908048839df92b63
jhin93 <scene1993@gmail.com>
2022-01-18 10:52:31 +0900

    인자를 다수 받을 수 있음. cout은 출력

--- cpp_work/HelloWorld.cpp
+++ cpp_work/HelloWorld.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
+// using namespace 선언은 말 그대로 소속을 알리는 역할
+// https://thinkpro.tistory.com/22
+using namespace std; //std는 standard의 약자. 이 문장은 표준 네임스페이스를 사용하겠다는 의미.
+// std라는 namespace를 사용할거라고 선언. 그러면 함수 내에서 명시적으로(std::) 안써도 됨.
+
 int main()
 {
-    std::cout << "Hello World!!" << std::endl;
+    cout << "Hello World!!" << endl;
 
     return 0;
     // HelloWorld를 실행한다. os가 받아준다. start-up 코드가 main을 받는 것.
---
+++ cpp_work/practice.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* []) // (인자의 개수, 인자를 받을 포인터)
+{
+    int num = 15;
+    cout << num << "\n"; // cout은 출력. num을 출력하고, 줄바꿈
+
+    return 0;
+}
+
+
+
+
+
