8a1b6d19cb920aac093948c3d86c22c72c5730c8
jhin93 <scene1993@gmail.com>
2022-01-18 12:20:58 +0900

    C++은 인자를 터미널에서 입력해 사용한다.

--- cpp_work/practice.cpp
+++ cpp_work/practice.cpp
@@ -2,15 +2,28 @@
 
 using namespace std;
 
-int main(int argc, char* []) // (인자의 개수, 인자를 받을 포인터)
+int main(int argc, char* argv[]) // (인자의 개수, 인자를 받을 포인터)
 {
     int num = 15;
     cout << num << "\n"; // cout은 출력. num을 출력하고, 줄바꿈
 
+    if(argc > 1)
+    {
+        cout << "argc = " << argc << endl;
+        for(int i = 0; i < argc; i++)
+        {
+            cout << "argv[" << i << "]" << "=" << argv[i] << endl;
+        }
+    }
     return 0;
 }
 
-
-
+// 터미널에서 입력 : ./practice hello world
+// 결과
+// 15
+// argc = 3
+// argv[0]=./practice
+// argv[1]=hello
+// argv[2]=world
 
 
