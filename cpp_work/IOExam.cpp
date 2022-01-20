96e7418cc7a8f602045e933b47da9451577c7274
jhin93 <scene1993@gmail.com>
2022-01-20 09:48:27 +0900

    입력해서 덧셈 출력 가능

--- cpp_work/IOExam.cpp
+++ cpp_work/IOExam.cpp
@@ -8,10 +8,15 @@ using namespace std;
 
 int main()
 {
-    int x = 0; // 항상 초기화를 해줘야 한다.
-    cout << "Input a number : " << x;
-    cin >> x; // 빌드 후 실행시켰을 때 콘솔창에서 직접 데이터를 입력
-    cout << "Your Input Number : " << x << endl;
+    int x, y; // 항상 초기화를 해줘야 한다.
+    int nSum;
+    cout << "Input a number : ";
+    cin >> x; cout << endl; // 빌드 후 실행시켰을 때 콘솔창에서 직접 데이터를 입력
+    cout << "Input y Number : ";
+    cin >> y; cout << endl;
+
+    nSum = x + y;
+    cout << "nSum = " << nSum << endl;
 
     return 0;
 }
