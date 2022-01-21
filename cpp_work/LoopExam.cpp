b5ecaa05ab11370c11364c1e55f44b158284016a
jhin93 <scene1993@gmail.com>
2022-01-21 09:41:16 +0900

    Range-Based for loop

--- cpp_work/LoopExam.cpp
+++ cpp_work/LoopExam.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -24,11 +25,24 @@ int main()
     //     cout << "for loop i = " << i << endl;
     // }
 
-    for(int n = 0, i = 100; n != i; ++n, --i) // 1)초기문이 2)조건문을 통과하고 나서 3)증감문을 적용.
+    // for(int n = 0, i = 100; n != i; ++n, --i) // 1)초기문이 2)조건문을 통과하고 나서 3)증감문을 적용.
+    // {
+    //     cout << "n = " << n << ", i = " << i << endl;
+    // }
+
+    int nArr[5] = {1, 3, 5, 7, 9};
+
+    for(int i : nArr)
     {
-        cout << "n = " << n << ", i = " << i << endl;
+        cout << "Range-Based for loop i = " << i << endl;
     }
 
+    string str ("Hello_World");
+    // char str[] = "Hello_world!!!";
+    for(char c : str)
+    {
+        cout << "Range-Based for loop c = " << c << endl;
+    }
 
     return 0;
 }
