551bfc9d1bf17c8793394f21e1af3979f12c6514
jhin93 <scene1993@gmail.com>
2022-01-21 10:45:01 +0900

    주석 추가

--- cpp_work/RefExam.cpp
+++ cpp_work/RefExam.cpp
@@ -11,7 +11,8 @@ int main()
 {
     string food = "Pizza";
     string &meal = food; // 주소값을 준 것
-    meal = "Hamburger"; // 같은 주소에 같은 데이터를 넣었기에 같은 값이 출력.
+    meal = "Hamburger"; // 같은 주소(meal과 food의 주소)에 같은 데이터를 넣었기에 food 값도 변경.
+    // meal = "Hamburger"; // 같은 주소(meal과 food의 주소)에 같은 데이터를 넣었기에 food 값도 변경.
 
     cout << food << ", Address of food : " << &food << endl;
     cout << meal << ", Address of meal : " << &meal << endl; // 같은 주소가 나옴
