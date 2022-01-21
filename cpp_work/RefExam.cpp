f0396c19832ddf125a5bed2e4035fd920075bf9b
jhin93 <scene1993@gmail.com>
2022-01-21 10:58:08 +0900

    주석 추가

--- cpp_work/RefExam.cpp
+++ cpp_work/RefExam.cpp
@@ -10,7 +10,7 @@ void abc(int &x)
 int main()
 {
     string food = "Pizza";
-    string &meal = food; // 주소값을 준 것
+    string &meal = food; // 주소값을 준 것. meal은 food를 바라보는 새로운 방식(같은 주소)
     meal = "Hamburger"; // 같은 주소(meal과 food의 주소)에 같은 데이터를 넣었기에 food 값도 변경.
     // meal = "Hamburger"; // 같은 주소(meal과 food의 주소)에 같은 데이터를 넣었기에 food 값도 변경.
 
