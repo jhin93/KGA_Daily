c1e8257accb6d65e3de3efa08e7c9e359cc1d58f
jhin93 <scene1993@gmail.com>
2022-01-21 10:23:27 +0900

    배열 다루기

--- cpp_work/ArrayExam.cpp
+++ cpp_work/ArrayExam.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -15,5 +16,21 @@ int main()
         // cout << grade[i] << endl; // 주소의 데이터를 나타냄. 85, 65, 90
         cout << &grade[i] << endl; // &는 주소를 나타내는(메모리를 참조하는) 참조 연산자
     }
+
+    string itCompany[] = {"Apple", "Microsoft", "IBM", "Amazon", "ESP Guitar"};
+    // string itCompany[5];
+    // itCompany[0] = "Apple";
+    // itCompany[1] = "Microsoft";
+    // itCompany[2] = "IBM";
+    // itCompany[3] = "Amazon";
+    // itCompany[4] = "ESP Guitar";
+    cout << itCompany[4] << endl;
+    itCompany[0] = "Snakebyte Guitar"; // 다시 초기화
+    for(string str : itCompany) // range-based
+    {
+        cout << str << endl;
+    }
+
+    return 0;
 
 }
