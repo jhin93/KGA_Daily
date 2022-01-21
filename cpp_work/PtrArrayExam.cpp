bc9614238b3319cd7d0ab6f2297e7d9e36e648fb
jhin93 <scene1993@gmail.com>
2022-01-21 17:05:35 +0900

    배열의 포인터를 활용한 구구단 완성

--- cpp_work/PtrArrayExam.cpp
+++ cpp_work/PtrArrayExam.cpp
@@ -7,14 +7,19 @@ int main()
     int arrNumber[5];
     int* ptr; // 포인터는 새로 주소를 할당하는게 가능 아래에서 확인 가능.
 
-    ptr = arrNumber, *ptr = 10; // arrNumber[0]
-    ptr++; *ptr = 20; // arrNumber[1]. 포인터가 두번째 방으로 이동해있는 상태
-    ptr = &arrNumber[2]; *ptr = 30; // 3번째 인덱스 주소를 입력해서 위치를 이동하고, 값을 대입
-    ptr = arrNumber + 3; *ptr = 40;  // 첫번째 주소 + 3 = 네번째 방. 값 대입
-    ptr = arrNumber; *(ptr + 4) = 50; // ptr을 첫번째 방(0번째 인덱스. arrNumber)으로 옮기고 4를 더함. 그리고 값 대입
+    // ptr = arrNumber, *ptr = 10; // arrNumber[0]
+    // ptr++; *ptr = 20; // arrNumber[1]. 포인터가 두번째 방으로 이동해있는 상태
+    // ptr = &arrNumber[2]; *ptr = 30; // 3번째 인덱스 주소를 입력해서 위치를 이동하고, 값을 대입
+    // ptr = arrNumber + 3; *ptr = 40;  // 첫번째 주소 + 3 = 네번째 방. 값 대입
+    // ptr = arrNumber; *(ptr + 4) = 50; // ptr을 첫번째 방(0번째 인덱스. arrNumber)으로 옮기고 4를 더함. 그리고 값 대입
+
+
 
     for(int i = 0; i < 5; i++)
+    {
+        ptr = &arrNumber[i]; *ptr = i * 10;
         cout << "[" << i << "] = " << arrNumber[i] << endl;
+    }
 
     return 0;
 }
---
+++ cpp_work/assignment/121gugudan2withPointer.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+// ptrArrayExam 응용
+
+// 구구단 가로세로 출력함수
+int main()
+{
+    // 입력받을 정수 초기화.
+    int inputNum = -1; // 처음에 초기화를 시킴
+    int numArr[10] = {1,2,3,4,5,6,7,8,9,10};
+    int* ptr = numArr;
+
+    while(true)
+    {
+        cout << "Enter number for multiplication table : ";
+        // 숫자 입력받기
+        cin >> inputNum;
+        cout << inputNum * 1 << " " << inputNum * 2 << " " << inputNum * 3 << " " << inputNum * 4 << " " << inputNum * 5 << " " << inputNum * 6 << " " << inputNum * 7 << " " << inputNum * 8 << " " << inputNum * 9 << endl;
+        if(inputNum != 0)
+        {
+            for(int i = 2; i < 10; i ++)
+            {
+                ptr = &numArr[i]; *ptr = i * inputNum;
+                cout << *ptr << "\r\n";
+            }
+        } else
+        {
+            cout << "done." << endl;
+            break;
+        }
+    }
+    return 0;
+}
+
+
+
