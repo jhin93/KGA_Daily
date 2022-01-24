2ae72c05a447c3c653235672cb2238dab12ef370
jhin93 <scene1993@gmail.com>
2022-01-24 11:54:55 +0900

    alloc 예제 주석 추가

--- cpp_work/DynamicMemExam.cpp
+++ cpp_work/DynamicMemExam.cpp
@@ -5,11 +5,23 @@ using namespace std;
 
 int main()
 {
+    /*
     int i;
     int arrLen = 3; //array길이를 3으로
     int* ptrArr;
+    // malloc() 함수
+    // ptrArr = (int*)malloc(arrLen * sizeof(int)); // dynamic allocation memory. 제각기 다른 값이 나옴.
+    // 예제
+    // int* pi = (int* p) malloc(sizeof(int) * 10);
+    // 정수 10개를 저장할 수 있는 메모리공간(즉 40바이트)를 할당하고 그 첫번째 주소를 int*로 형변환하여 pi에 저장
+    // char* pc
 
-    ptrArr = (int*)malloc(arrLen * sizeof(int)); // dynamic allocation memory
+    // calloc() 함수
+    ptrArr = (int*)calloc(arrLen, sizeof(int)); // 문법 : void *calloc(size_t nmemb, size_t size); 
+    // calloc함수(clear alloc)는 할당받은 데이터를 모두 0으로 만듬. 출력하면 0으로 나오는게 맞음
+
+    //realloc() 함수. 재할당하는 함수.
+
 
     if(ptrArr  == NULL) // dynamic allocation fail
     {
@@ -23,7 +35,25 @@ int main()
         cout << ptrArr[i] << endl;
     } 
 
-    free(ptrArr); // 다쓰고 free 적용
+    free(ptrArr); // 할당받은 메모리 해제
+    */
+
+    const char* pStr = "This is character string pointer"; // 초기화
+    char* pStrDest = (char*)calloc(strlen(pStr) + 1, sizeof(char)); // null까지 계산해서 +1
+
+    cout << "pStrDest Length : " << strlen(pStrDest) << endl; // strlen은 길이를 출력하는 메소드
+    strncpy(pStrDest, pStr, strlen(pStr) + 1); // strncpy - https://blockdmask.tistory.com/348 
+    cout << "pStrDest Length : " << strlen(pStrDest) << endl;
+    cout << "pStrDest : " << pStrDest << endl;
+
+    free(pStrDest); // 할당받은 메모리 해제
+
+    // 결과
+    /*
+    pStrDest Length : 0 - calloc 이라서 0
+    pStrDest Length : 32
+    pStrDest : This is character string pointer
+    */
 
     return 0;
 }
