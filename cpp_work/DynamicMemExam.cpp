21632bcad3bfeb637d318423c8eead0637070ebe
jhin93 <scene1993@gmail.com>
2022-01-24 12:14:36 +0900

    for문을 통해 주소를 계속 변형시키고, 연속적으로 다른 데이터들을 넣을 수 있음.

--- cpp_work/DynamicMemExam.cpp
+++ cpp_work/DynamicMemExam.cpp
@@ -38,15 +38,16 @@ int main()
     free(ptrArr); // 할당받은 메모리 해제
     */
 
-    const char* pStr = "This is character string pointer"; // 초기화
-    char* pStrDest = (char*)calloc(strlen(pStr) + 1, sizeof(char)); // null까지 계산해서 +1
+     
+    // const char* pStr = "This is character string pointer"; // 초기화
+    // char* pStrDest = (char*)calloc(strlen(pStr) + 1, sizeof(char)); // null까지 계산해서 +1
 
-    cout << "pStrDest Length : " << strlen(pStrDest) << endl; // strlen은 길이를 출력하는 메소드
-    strncpy(pStrDest, pStr, strlen(pStr) + 1); // strncpy - https://blockdmask.tistory.com/348 
-    cout << "pStrDest Length : " << strlen(pStrDest) << endl;
-    cout << "pStrDest : " << pStrDest << endl;
+    // cout << "pStrDest Length : " << strlen(pStrDest) << endl; // strlen은 길이를 출력하는 메소드
+    // strncpy(pStrDest, pStr, strlen(pStr) + 1); // strncpy - https://blockdmask.tistory.com/348 
+    // cout << "pStrDest Length : " << strlen(pStrDest) << endl;
+    // cout << "pStrDest : " << pStrDest << endl;
 
-    free(pStrDest); // 할당받은 메모리 해제
+    // free(pStrDest); // 할당받은 메모리 해제
 
     // 결과
     /*
@@ -55,5 +56,26 @@ int main()
     pStrDest : This is character string pointer
     */
 
+    char cAlphabet, *pAlphabet, *ptr;
+    pAlphabet = (char*)calloc(35, sizeof(char));
+
+    if(pAlphabet == NULL) // 실수를 방지하기 위한 로직
+    {
+        cout << "Dynamic memory allocation fail..." << endl;   
+        exit(1);
+    }
+
+    ptr = pAlphabet; // 주소를 그대로 준 것
+
+    for(cAlphabet = 0x41; cAlphabet < 0x5B; cAlphabet++)
+    {
+        *ptr++ = cAlphabet;
+    }
+    *ptr = '\0'; // 문자열의 마지막은 null이어야해서 \0을 넣음
+
+    cout << "Alphabet String : " << pAlphabet << endl; // *ptr은 null이 들어가서 찍어도 아무것도 안나옴. 그래서 연산에만 사용하고 pAlphabet을 찍음
+    // Alphabet String : ABCDEFGHIJKLMNOPQRSTUVWXYZ
+    
+    free(pAlphabet);
     return 0;
 }
