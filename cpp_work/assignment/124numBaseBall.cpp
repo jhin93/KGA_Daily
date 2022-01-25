6420fec523be028ac9beea8edd9238fb5b8b59be
jhin93 <scene1993@gmail.com>
2022-01-25 16:43:00 +0900

    숫자야구 완성 (난수 중복 허용)

--- cpp_work/assignment/124numBaseBall.cpp
+++ cpp_work/assignment/124numBaseBall.cpp
@@ -7,6 +7,8 @@ using namespace std;
 int main()
 {
     // 컴퓨터 난수 생성 (3자리). 중복없이 각 자리가 전부 다른수여야 함.
+    // 3자리 난수
+    int questRannum;
     const int questArrLength = 3; // 3자리
     int questArr[questArrLength] = {0, 0, 0};
 
@@ -16,14 +18,15 @@ int main()
     // random_device 를 통해 난수 생성 엔진을 초기화 한다.
     mt19937 gen(rd());
     // 0 부터 9 까지 균등하게 나타나는 난수열을 생성하기 위해 균등 분포 정의.
-    uniform_int_distribution<int> dis(0, 9);
+    uniform_int_distribution<int> dis(100, 999);
 
-    for(int i = 0; i < questArrLength; i ++)
+    questRannum = dis(gen); // 난수삽입
+    cout << "난수 : " << questRannum << endl;
+
+    for(int i = 2; i >= 0; i --)
     {
-        questArr[i] = dis(gen); // 난수 삽입
-        int *ranNum = &questArr[i];
-        cout << "각 자릿수" << *ranNum << endl;
-        cout << "각 자릿수 주소" << ranNum << endl;
+        questArr[i] = questRannum % 10;
+        questRannum /= 10;
     }
     
     // 사용자 입력수.
@@ -46,18 +49,37 @@ int main()
                 inputNum /= 10;
                 // cout << "test : " << inputArr[i] << " i : " << i << endl;
             }
-            break;
         }
+
+        int strike; // 맞으면 증가하는 strike 변수
+        int ball; // 맞으면 증가하는 ball 변수
+
+        // strike는 자릿수와 그 내용이 일치하는 개수를 체크
+        // 랜덤배열과 입력배열의 인덱스들의 주소의 데이터들을 비교할 것
+        // ball이 먼저 나오는 조건을 확인 -> 그 중에서 strike가 나오는 조건을 추가로 적용
+        for(int i = 0; i < questArrLength; i ++)
+        {
+            for(int j = 0; j < 3; j ++)
+            { // 랜덤배열 내부에 입력수배열의 원소가 있고, 자릿수가 다를 때 ball ++
+                if(j != i && questArr[i] == inputArr[j])
+                {
+                    ball++;
+                } else if(questArr[i] == inputArr[j] && j == i)
+                {
+                    strike++;
+                    if(strike == 3)
+                    {
+                        cout << "HomeRun!!" << endl;
+                    }
+                }
+            }
+        }
+        cout << "ball : " << ball << endl;
+        cout << "strike : " << strike << endl;
+
+        break;
+
     }
-
-    // 맞추면 증가하는 strike 변수, ball 변수
-    cout << "input number is : " << inputNum << endl;
-
-    int strike; // 맞으면 증가하는 strike 변수
-    int ball; // 맞으면 증가하는 ball 변수
-
-    // strike는 자릿수와 그 내용이 일치하는 개수를 체크
-    // 랜덤배열과 입력배열의 인덱스들의 주소의 데이터들을 비교할 것
     
     // 홈런을 확인하는 수식. strike가 홈런이면 alert 뜨면서 리로드
 
