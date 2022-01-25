2fcf227cbdc6dd23d3cb7dcd2189510672b39989
jhin93 <scene1993@gmail.com>
2022-01-25 12:07:43 +0900

    void 함수는 리턴을 하지 않는다

--- cpp_work/CPPFuncExam.cpp
+++ cpp_work/CPPFuncExam.cpp
@@ -19,6 +19,12 @@ void WhereAreYouFrom(string strName, int nAge, string strCountry = "KOREA")
     cout << "I came from " << strCountry << "!!!!\n";
 }
 
+int HowOldAreYou(int nAge)
+{
+    int age = nAge;
+
+    return age;
+}
 
 int main()
 {
@@ -30,5 +36,8 @@ int main()
     WhereAreYouFrom("SiEun", 20);
     WhereAreYouFrom("Hyejin", 24);
     WhereAreYouFrom("James Hetfield", 30, "USA");
+
+    cout << "My Age : " << HowOldAreYou(25) << endl;
+
     return 0;
 }
