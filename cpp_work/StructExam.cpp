6b774042cf9b6bb31144f1ec85fdaa7ec726b21d
jhin93 <scene1993@gmail.com>
2022-01-26 10:47:53 +0900

    book1은 포인터 사용, book2는 사용 X.Print..함수는 주소로받아서&book2

--- cpp_work/StructExam.cpp
+++ cpp_work/StructExam.cpp
@@ -14,7 +14,7 @@ int main()
 
     // 포인터로 사용가능. 동적할당.
     BooksInfo::Books* book1 = new BooksInfo::Books;
-    Books* book2 = new Books;
+    Books book2;
 
     strcpy(book1 -> title, "Learn C++ Programming 21 days");
     strcpy(book1 -> author, "Sieun Lee");
@@ -22,17 +22,17 @@ int main()
     book1 -> book_id = 12423;
     PrintBooksInformation(book1);
 
-    strcpy(book2 -> title, "Learn C++ Programming 21 days");
-    strcpy(book2 -> author, "Hyein Lee");
-    strcpy(book2 -> subject, "C++ Programming in Liquor");
-    book2 -> book_id = 44423;
-    PrintBooksInformation(book2);
+    strcpy(book2.title, "Learn C++ Programming 21 days");
+    strcpy(book2.author, "Hyein Lee");
+    strcpy(book2.subject, "C++ Programming in Liquor");
+    book2.book_id = 44423;
+    PrintBooksInformation(&book2); // 주소값을 넘겨줌
 
     // cout << "book1 title : " << book1.title << ", author : " << book1.author << ", subject : " << book1.subject << ", book1.book_id : " << book1.book_id << endl;
     // cout << "book2 title : " << book2.title << ", author : " << book2.author << ", subject : " << book2.subject << ", book2.book_id : " << book2.book_id << endl;
 
     delete book1;
-    delete book2;
+    // delete book2;
 
     return 0;
 }
