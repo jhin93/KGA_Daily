b9e682e293db359bd696190c9e039f3775398cb0
jhin93 <scene1993@gmail.com>
2022-01-26 11:35:42 +0900

    pragma pack push, pragma pack pop

--- cpp_work/StructSizeExam.cpp
+++ cpp_work/StructSizeExam.cpp
@@ -2,7 +2,16 @@
 
 using namespace std;
 
+#pragma pack (push,1) // 가장 큰 멤버변수의 메모리 크기를 기준으로 패딩을 결정하지 않고, 1byte를 기본 단위로 사용하겠다는 말.
 struct ExamSize
+{
+    char a; // 1byte
+    int b; // 4byte
+    double c; // 8byte. 총 13byte
+};
+#pragma pack (pop) // pragma pack으로 결정해준 메모리 기본 단위를 해제.
+
+struct ExamSize1
 {
     char a; // 1byte
     int b; // 4byte
@@ -15,5 +24,6 @@ int main(int argc, char* argv[])
     cout << "char : " << sizeof(char) << ", " << sizeof(int) << ", " << sizeof(double) << endl;
     
     cout << "Size of structure : " << sizeof(ExamSize) << endl;
+    cout << "Size of structure : " << sizeof(ExamSize1) << endl;
     return 0;
 }
