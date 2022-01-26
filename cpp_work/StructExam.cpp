61e644b1d35ada01c03f27eacd7d5894c7c1d69b
jhin93 <scene1993@gmail.com>
2022-01-26 10:28:00 +0900

    포인터(동적할당)로 적용도 가능. '.' 를 -> 로 교체해서 사용해야 함

--- cpp_work/StructExam.cpp
+++ cpp_work/StructExam.cpp
@@ -10,32 +10,42 @@ struct Books
     int book_id;
 };
 
-void PrintBooksInformation(struct Books book); // 함수 프로토타입 선언
+void PrintBooksInformation(Books* book); // 함수 프로토타입 선언
 
 int main()
 {
-    struct Books book1;
-    struct Books book2;
+    // struct Books book1;
+    // struct Books book2;
 
-    strcpy(book1.title, "Learn C++ Programming 21 days");
-    strcpy(book1.author, "Sieun Lee");
-    strcpy(book1.subject, "C++ Programming in Liquor");
-    book1.book_id = 12423;
+    // Books book1; struct 생략가능
+    // Books book2;
+
+    // 포인터로 사용가능. 동적할당.
+    Books* book1 = new Books;
+    Books* book2 = new Books;
+
+    strcpy(book1 -> title, "Learn C++ Programming 21 days");
+    strcpy(book1 -> author, "Sieun Lee");
+    strcpy(book1 -> subject, "C++ Programming in Liquor");
+    book1 -> book_id = 12423;
     PrintBooksInformation(book1);
 
-    strcpy(book2.title, "Learn C++ Programming 21 days");
-    strcpy(book2.author, "Hyein Lee");
-    strcpy(book2.subject, "C++ Programming in Liquor");
-    book2.book_id = 44423;
+    strcpy(book2 -> title, "Learn C++ Programming 21 days");
+    strcpy(book2 -> author, "Hyein Lee");
+    strcpy(book2 -> subject, "C++ Programming in Liquor");
+    book2 -> book_id = 44423;
     PrintBooksInformation(book2);
 
     // cout << "book1 title : " << book1.title << ", author : " << book1.author << ", subject : " << book1.subject << ", book1.book_id : " << book1.book_id << endl;
     // cout << "book2 title : " << book2.title << ", author : " << book2.author << ", subject : " << book2.subject << ", book2.book_id : " << book2.book_id << endl;
 
+    delete book1;
+    delete book2;
+
     return 0;
 }
 
-void PrintBooksInformation(struct Books book)
+void PrintBooksInformation(struct Books* book)
 {
-    cout << "book title : " << book.title << ", author : " << book.author << ", subject : " << book.subject << ", book.book_id : " << book.book_id << endl;
+    cout << "book title : " << book -> title << ", author : " << book -> author << ", subject : " << book -> subject << ", book -> book_id : " << book -> book_id << endl;
 }
