2afba79a87cd31691c35f6cad326ae8e4ebac994
jhin93 <scene1993@gmail.com>
2022-01-26 10:34:52 +0900

    Struct만 헤더 파일로 빼서 include해서 사용

--- cpp_work/StructExam.cpp
+++ cpp_work/StructExam.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
+#include "StructExam.hpp"
 
 using namespace std;
-
-struct Books
-{
-    char title[50];
-    char author[50];
-    char subject[100];
-    int book_id;
-};
 
 void PrintBooksInformation(Books* book); // 함수 프로토타입 선언
 
