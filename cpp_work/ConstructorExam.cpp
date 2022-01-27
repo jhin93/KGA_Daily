530dc80016d62f55f9aad2364560a3b9e808c9b1
jhin93 <scene1993@gmail.com>
2022-01-27 09:41:12 +0900

    객체 생성자 인자 활용 기초예시

---
+++ cpp_work/ConstructorExam.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class CCar {
+    public:
+        CCar() {}; // Default constructor. 객체 생성자 선언
+        CCar(string brand, string name, int year)
+        {
+            m_strBrand = brand;
+            m_strName = name;
+            m_nYear = year;
+        }
+        // 속성값 선언
+        string m_strBrand; 
+        string m_strName;
+        int m_nYear;
+};
+
+int main()
+{
+    CCar bmwCar("BMW", "X5", 2002);
+    CCar audiCar("Audi", "A8", 2012);
+
+    cout << bmwCar.m_strBrand << " : " << bmwCar.m_strName << ", " << bmwCar.m_nYear << endl;
+    cout << audiCar.m_strBrand << " : " << audiCar.m_strName << ", " << audiCar.m_nYear << endl;
+
+    return 0;
+};
