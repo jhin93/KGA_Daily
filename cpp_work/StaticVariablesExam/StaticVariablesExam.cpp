dd79819aec6faf27def9adf4946c341674b2ad27
jhin93 <scene1993@gmail.com>
2022-01-28 12:49:56 +0900

    설명추가

--- cpp_work/StaticVariablesExam/StaticVariablesExam.cpp
+++ cpp_work/StaticVariablesExam/StaticVariablesExam.cpp
@@ -10,7 +10,7 @@ int main()
     CPerson* pKirk = new CPerson("Kirk Hemett", 60);
     CPerson* pRobert = new CPerson("Robert Truejilo", 58);
 
-    cout << "Currently Created People : " << CPerson::PersonCount() << endl;
+    cout << "Currently Created People : " << CPerson::PersonCount() << endl; // static이기에 하나만 호출해도 4개의 결과가 호출된다. 그래도 private로 들어가면 이렇게 못씀
 
     if(pLas != NULL) delete pLas;
     if(pJames != NULL) delete pJames;
