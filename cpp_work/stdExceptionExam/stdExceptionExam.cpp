f0410a6583569057fc4f65cd21056548023143f5
jhin93 <scene1993@gmail.com>
2022-02-03 12:52:52 +0900

    스스로 에러를 정의할 수도 있다.

--- cpp_work/stdExceptionExam/stdExceptionExam.cpp
+++ cpp_work/stdExceptionExam/stdExceptionExam.cpp
@@ -9,22 +9,46 @@ void ExceptHandler(){
     exit(-1); // 프로그램 강제종료
 };
 
+struct MyException : public exception
+{
+    const char* what() const throw()
+    {
+        return "Defining new exception";
+    }
+};
+
 int main()
 {
-    set_terminate(ExceptHandler); // 에러핸들러 연결
+    // set_terminate(ExceptHandler); // 에러핸들러 연결
+    // try
+    // {
+    //     throw 1;
+    // }
+    // catch(char* const ex)
+    // {
+    //     // cannot process integer exception
+    // }
+    // cout << "Hello world" << endl; 
+
+    // // 결과
+    // // Exception!!!
+    // // Hello World는 뜨지 않는다. exit -1로 프로그램이 강제 종료되었기 때문
+    
     try
     {
-        throw 1;
+        throw MyException();
     }
-    catch(char* const ex)
+    catch (MyException& e)
     {
-        // cannot process integer exception
+        cout << "MyException Caught" << endl;
+        cout << e.what() << endl;
     }
+    catch(const std::exception& e)
+    {
+        //other error
+    }
+    
     cout << "Hello world" << endl; 
 
-    // 결과
-    // Exception!!!
-    // Hello World는 뜨지 않는다. exit -1로 프로그램이 강제 종료되었기 때문
-    
     return 0;
 }
