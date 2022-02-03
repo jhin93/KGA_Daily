52f57139e44aca6ac67bebdcec5e29431068e0ec
jhin93 <scene1993@gmail.com>
2022-02-03 12:31:13 +0900

    나눗셈 에러 체크

--- cpp_work/VerySimpleThrowExam/VerySimpleThrowExam.cpp
+++ cpp_work/VerySimpleThrowExam/VerySimpleThrowExam.cpp
@@ -2,15 +2,36 @@
 
 using namespace std;
 
+double division(int a, int b)
+{
+    if(b == 0)
+    {
+        throw "Division by Zero!!!";
+    }
+
+    return (double)(a/b);
+}
+
 int main()
 {
+
+    int x = 50;
+    // int y = 0;
+    int y = 25;
+
+    double z = 0.0f; // f는 실수를 나타냄
+
     try
     {
-        throw 128;
+        // throw 128;
+        z = division(x, y);
+        cout << "divided value : " << z << endl;
     }
-    catch(int errCode)
+    // catch(int errCode)
+    catch(const char* errMsg)
     {
-        cout << "An exception occured. Exception Error code : " << errCode;
+        // cout << "An exception occured. Exception Error code : " << errCode;
+        cerr << errMsg << endl; // cerr.
     }
     
     return 0;
