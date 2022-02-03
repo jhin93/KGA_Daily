4fddf2ed3ab334406dfbbb5e99adab1b5be983e5
jhin93 <scene1993@gmail.com>
2022-02-03 12:39:51 +0900

    주석추가

--- cpp_work/VerySimpleThrowExam/VerySimpleThrowExam.cpp
+++ cpp_work/VerySimpleThrowExam/VerySimpleThrowExam.cpp
@@ -16,8 +16,8 @@ int main()
 {
 
     int x = 50;
-    // int y = 0;
-    int y = 25;
+    int y = 0;
+    // int y = 25;
 
     double z = 0.0f; // f는 실수를 나타냄
 
@@ -33,6 +33,15 @@ int main()
         // cout << "An exception occured. Exception Error code : " << errCode;
         cerr << errMsg << endl; // cerr.
     }
+    // try
+    // {
+    //     z = division(x, y);
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     cout << e.what() << endl;
+    // }
+    
     
     return 0;
 }
