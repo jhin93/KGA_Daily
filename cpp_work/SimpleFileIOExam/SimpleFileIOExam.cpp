4f0579fc52990533f639e8a2cc05b52252f16ee4
jhin93 <scene1993@gmail.com>
2022-02-03 10:10:34 +0900

    ifstream 사용예시(파일 읽기)

--- cpp_work/SimpleFileIOExam/SimpleFileIOExam.cpp
+++ cpp_work/SimpleFileIOExam/SimpleFileIOExam.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    ofstream file("simplefile.txt");
+    // ofstream file("simplefile.txt");
 
-    file << "Hello world\n";
+    // file << "Hello world\n";
 
-    file.close();
+    // file.close();
+
+    string readString;
+    ifstream readFile("simplefile.txt");
+    
+    while(getline(readFile, readString))
+    {
+        cout << readString;
+    }
 
     return 0;
 }
