b1837a0e2995de0113a92deff6f6b9d288c1d3eb
jhin93 <scene1993@gmail.com>
2022-02-04 14:32:59 +0900

    코드수정. {} 추가

--- cpp_work/ListExam/ListExam.cpp
+++ cpp_work/ListExam/ListExam.cpp
@@ -48,8 +48,11 @@ int main()
     for(list<int>::iterator itr = nList.begin(); itr != nList.end(); ++itr)
     {
         if(*itr == 30)
+        {
             nList.erase(itr); // 30을 삭제
             break; // 원래는 삭제하고 나서도 반복문이 돌기 때문에 삭제로 인한 공간때문에 에러가 나야함. 근데 안나는게 이상함. C++ 버전 차이거나 vscode - vs 차이일지도.
+        }
+        
     }
     cout << "element value 30 deleted" << endl;
     PrintList(nList);
