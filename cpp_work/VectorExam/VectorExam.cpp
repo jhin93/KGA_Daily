fbff692c2e625e660e4cd093c3c00a6f0b3ceffe
jhin93 <scene1993@gmail.com>
2022-02-04 11:08:16 +0900

    벡터 사용 예제

---
+++ cpp_work/VectorExam/VectorExam.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> vec; //interger 형 vector
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+    vec.push_back(40);
+
+    for(vector<int>::size_type i = 0; i < vec.size(); i ++)
+    {
+        cout << "vec " << i << " : " << vec[i] << endl; 
+    }
+
+    return 0;
+}
