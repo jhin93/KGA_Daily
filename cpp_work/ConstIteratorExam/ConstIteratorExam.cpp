e3fa5bdaaccef038510ce6834a9643b23a09e8fa
jhin93 <scene1993@gmail.com>
2022-02-04 12:51:57 +0900

    reverse itertor 사용예제 + auto

--- cpp_work/ConstIteratorExam/ConstIteratorExam.cpp
+++ cpp_work/ConstIteratorExam/ConstIteratorExam.cpp
@@ -15,6 +15,15 @@ void PrintVector(vector<T> &vc) // 벡터 출력함수
   cout << "]\n";
 }
 
+template <typename T>
+void print_vector_range_based(vector<T>& vec)
+{
+  for(const auto& elem : vec)
+  {
+    cout << "auto using : " << elem << endl;
+  }
+}
+
 int main()
 {
     vector<int> vec;
@@ -45,8 +54,32 @@ int main()
     {
       cout << *ritr << " ";
     }
-    cout << "]";
+    cout << "]" << endl;
 
+
+    // 새로운 벡터 선언
+    vector<int> nVector;
+    nVector.push_back(1);
+    nVector.push_back(2);
+    nVector.push_back(3);
+    // reverse iterator를 사용하지 않는 비효율적인 예제
+    // for(vector<int>::size_type i = nVector.size() - 1; i >= 0; i --)
+    // {
+    //   cout << nVector[i] << endl;
+    // }
+
+    for(int elem : nVector) // range based for문
+    {
+      cout << "element : " << elem << endl;
+    }
+
+    // auto는 range based 에서 사용가능하다. 모호한 변수를 반복문 돌릴때 사용한다.
+    print_vector_range_based(nVector);
+
+    auto nNum = 10;
+    auto strHello = "Hello";
+
+    cout << "nNum : " << nNum << ", strHello : " << strHello << endl;
 
     return 0;
 }
