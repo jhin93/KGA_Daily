81cb59627c55855f1f2fb59faca6c99e7cf320d6
jhin93 <scene1993@gmail.com>
2022-02-04 11:21:33 +0900

    iterator 사용 예제

--- cpp_work/VectorExam/VectorExam.cpp
+++ cpp_work/VectorExam/VectorExam.cpp
@@ -11,10 +11,19 @@ int main()
     vec.push_back(30);
     vec.push_back(40);
 
-    for(vector<int>::size_type i = 0; i < vec.size(); i ++)
+    // for(vector<int>::size_type i = 0; i < vec.size(); i ++)
+    // {
+    //     cout << "vec " << i << " : " << vec[i] << endl; 
+    // }
+
+    for(vector<int>::iterator itr = vec.begin(); itr != vec.end(); ++itr) // 변수명을 itr로 보통 많이 쓴다. itr != vec.end() - 즉 벡터의 마지막 원소까지를 범위로
     {
-        cout << "vec " << i << " : " << vec[i] << endl; 
-    }
+        cout << "vector itr : " << *itr << endl;
+    } 
+    
+    // 특정값(3번째)출력
+    vector<int>::iterator itr = vec.begin() + 2;
+    cout << "third element : " << *itr << endl;
 
     return 0;
 }
