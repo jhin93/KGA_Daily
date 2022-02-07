468c96c0e45f27d40f43a8465b1f2f11799504b3
jhin93 <scene1993@gmail.com>
2022-02-07 11:27:11 +0900

    map 사용 예제. 키값으로 정렬

---
+++ cpp_work/MapExam/MapExam.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+template <typename K, typename V>
+void PrintMapElement(map<K, V>& m)
+{
+    for(auto itr = m.begin(); itr != m.end(); ++itr)
+    {
+        cout << itr -> first << ", " << itr -> second; // first가 키, second가 밸류
+    }
+}
+
+int main()
+{
+    map<string, double> pitcherList;
+    pitcherList.insert(pair<string, double>("Kershaw", 0.12));
+    pitcherList.insert(pair<string, double>("DDung", 7.89));
+    pitcherList.insert(pair<string, double>("Otani", 5.34));
+
+    pitcherList.insert(make_pair("Chango", 2.12));
+    pitcherList.insert(make_pair("SunWoo", 3.14));
+    pitcherList.insert(make_pair("KwangHyun", 3.55));
+
+    pitcherList["DongRyul"] = 3.45;
+    pitcherList["DongWon"] = 3.45;
+    pitcherList["ChooChoo"] = 2.51;
+
+    PrintMapElement(pitcherList);
+
+    cout << "Chanho's ERA : " << pitcherList["Chanho"] << endl;
+
+    return 0;
+}
