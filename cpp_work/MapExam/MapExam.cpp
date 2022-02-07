9ea23c9f2d32d21539f1647a245686bff964160d
jhin93 <scene1993@gmail.com>
2022-02-07 11:42:04 +0900

    map은 키값이 없을 때 그냥 추가해버리기에 먼저 find를 해줘야 한다.

--- cpp_work/MapExam/MapExam.cpp
+++ cpp_work/MapExam/MapExam.cpp
@@ -13,6 +13,20 @@ void PrintMapElement(map<K, V>& m)
     }
 }
 
+template <typename K, typename V>
+void SearchPrintElement(map<K, V>& m, K key)
+{
+    auto itr = m.find(key); // map은 오타가 나도 에러를 만들어내는게 아니라 그냥 새로운 키로 넣어버리고 밸류는 0을 할당해버린다. 항상 find로 값을 체크할 것.
+    if(itr != m.end())
+    {
+        cout << key << " => " << itr -> second << endl; // value는 itr -> second
+    }
+    else
+    {
+        cout << key << " is not found in map" << endl;
+    }
+}
+
 int main()
 {
     map<string, double> pitcherList;
@@ -20,7 +34,7 @@ int main()
     pitcherList.insert(pair<string, double>("DDung", 7.89));
     pitcherList.insert(pair<string, double>("Otani", 5.34));
 
-    pitcherList.insert(make_pair("Chango", 2.12));
+    pitcherList.insert(make_pair("Chanho", 2.12)); 
     pitcherList.insert(make_pair("SunWoo", 3.14));
     pitcherList.insert(make_pair("KwangHyun", 3.55));
 
@@ -33,5 +47,9 @@ int main()
     cout << "Chanho's ERA : " << pitcherList["Chanho"] << endl;
     cout << "ChooChoo's ERA : " << pitcherList["ChooChoo"] << endl;
 
+    SearchPrintElement(pitcherList, string("Chaboom")); // 찾을 키값의 타입을 명시해야 한다(string이라고 타입변환). K key가 뭔지 모르니까.
+    // 결과 : Chaboom is not found in map
+    SearchPrintElement(pitcherList, string("Otani"));
+    // 결과 : Otani => 5.34
     return 0;
 }
