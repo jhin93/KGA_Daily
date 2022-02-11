f90c163367a78ee999ec8a97eb0fc7594b12eb98
jhin93 <scene1993@gmail.com>
2022-02-11 09:47:57 +0900

    case 입력받아서 출력하는 case문

---
+++ linux/casearg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+option="${1}"
+case ${option} in
+	-f) FILE="${2}"
+		echo "File name is $FILE"
+	;;
+	-d) DIR="${2}"
+		echo "Directory name is $DIR"
+	;;
+	*)
+		echo "`basename ${0}`:usage: [-f file] | [-d dirname]"	
+		exit 1 # Command to come out of the program with status 1
+	;;
+esac
