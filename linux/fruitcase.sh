28fddadb9b0cca4df4174e9e38c10f7a5447ed9c
jhin93 <scene1993@gmail.com>
2022-02-11 09:36:07 +0900

    리눅스 case 문

---
+++ linux/fruitcase.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FRUIT="banana"
+
+case "$FRUIT" in
+	"apple") echo "Apple pie is quite tasty"
+	;;
+	"banana") echo "I like banana nut bread."
+	;;
+	"strawberry") echo "I like strawberry milk"
+	;;
+esac
