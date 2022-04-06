8a54b6548c0aaf3086c32dcb7bf4db694977c578
jhin93 <scene1993@gmail.com>
2022-04-06 10:36:06 +0900

    배포 순서도 상관이 있다.

--- solidity/MappingExam.sol
+++ solidity/MappingExam.sol
@@ -13,6 +13,7 @@ contract MappingUser {
         MappingExample m = new MappingExample();
         m.update(100);
 
-        return m.balances(this);
+        return m.balances(this); // 에러발생 : Invalid type for argument in function call. Invalid implicit conversion from contract MappingUser to address requested.
+        // 리믹스에선 MappingUser를 먼저 배포하고, MappingExample를 배포한 후 update에 777을 입력해주면 새 주소(new MappingExample();)의 밸런스가 바뀜
     }
 }
