08814256e2211f878d0f60ccafdad94c2d1d2cc3
jhin93 <scene1993@gmail.com>
2022-04-06 10:14:26 +0900

    매핑 예제

---
+++ solidity/MappingExam.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.13;
+
+contract MappingExample {
+    mapping(address => uint) public balances;
+
+    function update(uint newBalances) public {
+        balances[msg.sender] = newBalances;
+    }
+}
+
+contract MappingUser {
+    function f() public returns(uint) {
+        MappingExample m = new MappingExample();
+        m.update(100);
+
+        return m.balances(this);
+    }
+}
