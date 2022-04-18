f3be5bf8e9a3a99e30fd0b98590b157410ed0cc9
jhin93 <scene1993@gmail.com>
2022-04-18 15:42:19 +0900

    쿠키를 쓰는 이유

--- leveltest0414/leveltestWeb_Q3.js
+++ leveltest0414/leveltestWeb_Q3.js
@@ -3,12 +3,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/getCookie', (req, res) => {
+app.get('/', (req, res) => {
   console.log(req.headers) // req는 request message라는 브라우저가 쏴준 메시지를 몽땅 담고 있는 것.
   // 헤더조작
   res.setHeader('Set-Cookie', 'token=true') // 크롬 검사기 - 애플리케이션 - 쿠키
   // 헤더에서도 확인 가능 Set-Cookie: token=true
   res.send('lvtest q3')
+})
+
+app.get('/getCookie', (req, res) => {
+  const [name, value] = req.headers.cookie.split('=')
+  console.log(name, value) // token true 이렇게 나온다. 이걸 라이브러리 쿠키 파서로 가능하다.
+  res.send("Hello")
 })
 
 app.listen(port, () => {
--- leveltest0414/leveltestWeb_Q4.js
+++ leveltest0414/leveltestWeb_Q4.js
@@ -1,9 +1,30 @@
+// 서버가 2개. 프론트 서버 , 백엔드 서버
+// 브라우저 <-> 프론트 A(4000)
+// 브라우저 <-> 백 B(3000)
+
+// 브라우저는 기본적으로 보안이라는 개념이 있고, 허락을 따로 해줘야 한다.
+// 헤더부분을 따로 조작하면 cors 따로 해결 가능
+
+// 쿠키로 로그인해야 하는 이유.
+// 세션으로 로그인 하면 그에 해당하는 서버만 로그인이 가능하다.
+// 하지만 쿠키로 로그인을 하게 되면 서버가 증설(ex 사용자 증가 등의 이유로)이 되어도 브라우저에서 가지고 있는 쿠키로 어느 서버건 로그인이 가능하다.
+// 이 쿠키를 잘 쓰기 위해 등장한게 jwt.
+
+// 로그인 전문 서버가 OAuth.
+
 const express = require('express')
 const app = express()
 const port = 4000
+const axios = require('axios')
 
 app.get('/', (req, res) => {
   res.send('lvtest q4')
+})
+
+axios.post("http://localhost:4000/getCookie", {name: "ingoo"}, {
+  headers : {
+    'Content-type' : 'application/x-www-form-urlencoded'
+  }
 })
 
 app.listen(port, () => {
