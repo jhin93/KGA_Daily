b3a42c14225e378b82ad612b28e4300f48fbbee2
jhin93 <scene1993@gmail.com>
2022-04-19 12:45:30 +0900

    화살표 이동 완료

--- Javascript/new/220419.js
+++ Javascript/new/220419.js
@@ -6,38 +6,75 @@
 
 const canvas = document.getElementById('myCanvas');
 const context = canvas.getContext('2d');
+const arcRadius = 20; // 원 반지름
 
-let arcPosX = canvas.width/2;
+// 원 관련 변수
+let arcPosX = canvas.width/2 + 120;
 let arcPosY = canvas.height/2;
 let arcMoveDirX = -1;
 let arcMoveDirY = -1;
 let arcMoveSpeed = 2;
 
+// 사각형 바 관련 변수
+const barWidth = 100;
+const barHeight = 20;
+let barPosX = canvas.width/2 - barWidth/2;
+let barPosY =  canvas.height - barHeight;
+let barMoveSpeed = 30;
+
 
 // 실습. 동그라미가 오른쪽으로 움직이다가 캔버스 끝에 닿으면 왼쪽으로 이동.
 // 왼쪽으로 이동하다가 오른쪽에 닿으면 다시 반대로 이동.
 
+document.addEventListener('keydown', keyDownEventHandler); // keydown이라는 이벤트가 발생하면 함수를 호출한다
+// document.addEventListener('keyup', keyUpEventHandler); // keydown이라는 이벤트가 발생하면 함수를 호출한다
 
+
+
+// 키보드 조작
+
+function keyDownEventHandler(e) { // 무슨키를 눌렀는지 알기 위해 매개변수 e 대입
+    if (e.key == 'ArrowRight') {
+        // 바를 오른쪽으로 이동
+        // console.log("ArrowRight 는", e.key)
+        barPosX += barMoveSpeed
+    }
+    else if (e.key == 'ArrowLeft') {
+        // 바를 왼쪽으로 이동
+        // console.log("ArrowLeft 는", e.key)
+        barPosX -= barMoveSpeed
+    }
+}
+
+
+
+
+
+// 도형 움직이기
+
+
+function barUpdate() {
+    
+}
 
 
 function update() {
     // 데이터 수정(도형의 위치 이동)
-    if(arcPosX - 50 < 0) {
+    if(arcPosX - arcRadius < 0) {
         arcMoveDirX = 1
-    } else if (arcPosX + 50 > canvas.width) {
+    } else if (arcPosX + arcRadius > canvas.width) {
         arcMoveDirX = -1
     }
 
-    if(arcPosY - 50 < 0){
+    if(arcPosY - arcRadius < 0){
        arcMoveDirY = 1; 
-    } else if(arcPosY + 50 > canvas.height) {
+    } else if(arcPosY + arcRadius > canvas.height) {
         arcMoveDirY = -1;
     }
 
-    arcPosX += arcMoveDirX * arcMoveSpeed; // 원 움직이기. 
-    arcPosY += arcMoveDirY * arcMoveSpeed; // 위아래
+    arcPosX += arcMoveDirX * arcMoveSpeed; // 원 좌우로 움직이기. 
+    arcPosY += arcMoveDirY * arcMoveSpeed; // 원 위아래로 움직이기
 }
-
 
 
 
@@ -58,7 +95,7 @@ function drawRect() {
     context.beginPath(); // 그리기 시작
 
 
-    context.rect(canvas.width/2 - 100/2, canvas.height/2 - 100/2, 100, 100); // rect는 좌상단 기준으로 그리기
+    context.rect(barPosX, barPosY, barWidth, barHeight); // rect는 좌상단 기준으로 그리기
     context.fillStyle = 'red';
     context.fill();
 
@@ -70,7 +107,7 @@ function drawRect() {
 function drawArc(){
     context.beginPath(); // 그리기 시작
 
-    context.arc(arcPosX, arcPosY, 50, 0, 2 * Math.PI)
+    context.arc(arcPosX, arcPosY, arcRadius, 0, 2 * Math.PI)
     context.fillStyle = 'blue';
     context.fill();
     
