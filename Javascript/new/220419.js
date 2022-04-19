e88ff0751ae8becf2e73df6f59a0e199b49c7e85
jhin93 <scene1993@gmail.com>
2022-04-19 11:16:03 +0900

    화면 그리기. 잔상이 남지 않도록, setInterval 함수

--- Javascript/new/220419.js
+++ Javascript/new/220419.js
@@ -7,21 +7,63 @@
 const canvas = document.getElementById('myCanvas');
 const context = canvas.getContext('2d');
 
+let arcPosX = canvas.width/2;
+let arcPosY = canvas.height/2;
+
+
+
 context.clearRect(0, 0, canvas.width, canvas.height);
 
-context.beginPath(); // 그리기 시작
 
 
-context.rect(canvas.width/2 - 100/2, canvas.height/2 - 100/2, 100, 100);
-context.fillStyle = 'red';
-context.fill();
 
-context.closePath(); // 그리기 종료
+function update() {
+    // 데이터 수정(도형의 위치 이동)
+    arcPosX++; // 원 움직이기
+}
 
-context.beginPath(); // 그리기 시작
 
-context.arc(canvas.width/2 - 100/2, canvas.height/2 - 100/2, 50, 0, 2 * Math.PI)
-context.fillStyle = 'blue';
-context.fill();
 
-context.closePath(); // 그리기 종료
+
+
+// 그리기
+
+
+function draw() {
+    // 화면 클리어
+    context.clearRect(0, 0, canvas.width, canvas.height); // 움직이고 이전 상태는 지운다. 아니면 잔상이 남음
+    // 다른 도형 그리기
+    drawRect();
+    drawArc();
+}
+
+// 사각형 그리는 함수
+function drawRect() {
+    context.beginPath(); // 그리기 시작
+
+
+    context.rect(canvas.width/2 - 100/2, canvas.height/2 - 100/2, 100, 100); // rect는 좌상단 기준으로 그리기
+    context.fillStyle = 'red';
+    context.fill();
+
+    context.closePath(); // 그리기 종료
+
+}
+
+// 원 그리는 함수
+function drawArc(){
+    context.beginPath(); // 그리기 시작
+
+    context.arc(arcPosX, arcPosY, 50, 0, 2 * Math.PI)
+    context.fillStyle = 'blue';
+    context.fill();
+    
+    context.closePath(); // 그리기 종료
+}
+
+
+// 지속적인 변화주기 setInterval. 함수이름, 시간, 
+setInterval(update, 10);
+setInterval(draw, 10);
+
+
