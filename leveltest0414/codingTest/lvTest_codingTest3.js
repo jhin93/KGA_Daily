fa81838743327e195579a01e924bce91d77014ce
jhin93 <scene1993@gmail.com>
2022-04-19 17:42:30 +0900

    교수님 풀이

--- leveltest0414/codingTest/lvTest_codingTest3.js
+++ leveltest0414/codingTest/lvTest_codingTest3.js
@@ -11,39 +11,71 @@ let resultArr = []; // 소수 만드는 배열을 모아놓은 배열
 let primeNum; // 소수를 담을 변수
 
 // 소수 확인 함수
-function checkPrimeNum (num) {
-    if (num == 2 || num == 3 || num == 5 || num == 7) {
-        return "primeNumber"
-    } else if (num % 2 != 0 && num % 3 != 0 && num % 5 != 0 && num % 7 != 0) {
-        return "primeNumber"
-    }
+// function checkPrimeNum (num) {
+//     if (num == 2 || num == 3 || num == 5 || num == 7) {
+//         return "primeNumber"
+//     } else if (num % 2 != 0 && num % 3 != 0 && num % 5 != 0 && num % 7 != 0 && num != 1) {
+//         return "primeNumber"
+//     }
+// }
+
+// function solution(nums) {
+//     // 3중 반복문. 
+//     for(let i = 0; i < nums.length; i++){
+//         for(let j = 1; j < nums.length; j++){
+//             for(let k = 2; k < nums.length; k++){
+//                 primeNum = nums[i] + nums[j] + nums[k]; // 일단 더한 값을 변수에 담기
+//                 if(k == nums.length - 1){ // k가 배열의 끝에 도달했을 때 맨 앞 요소를 제거하고 다시 로직 반복
+//                     nums.shift();
+//                 }
+//                 if(checkPrimeNum(primeNum) == "primeNumber"){ // checkPrimeNum로 소수인 상황을 전제로 둠.
+//                     primeNumArr.push(nums[i], nums[j], nums[k]) // 소수를 만드는 3개의 수를 담는 배열
+//                     resultArr.push(primeNumArr) // 소수 만드는 배열들을 하나의 배열에 모은다.
+//                 }
+//             }
+//         }
+//     }
+//     let result = resultArr.length;
+//     return result;
+// }
+
+// [1,2,3,4] 대입시 1 출력되어야함
+// [1,2,7,6,4] 대입시 4 출력되어야함
+
+
+
+//교수님 풀이
+
+function checkPrimeNum(num) {
+    // 17이 있을 때, 1, 17로만 나누어 떨어지는 숫자
+    for(let i = 2; i < num; i ++){ // 2부터 num 사이
+        if(num % i == 0) return false
+    } 
+    return true;
 }
 
 function solution(nums) {
-    // 3중 반복문. 
-    for(let i = 0; i < nums.length; i++){
-        for(let j = 1; j < nums.length; j++){
-            for(let k = 2; k < nums.length; k++){
-                primeNum = nums[i] + nums[j] + nums[k]; // 일단 더한 값을 변수에 담기
-                if(k == nums.length - 1){ // k가 배열의 끝에 도달했을 때 맨 앞 요소를 제거하고 다시 로직 반복
-                    nums.shift();
-                }
-                if(checkPrimeNum(primeNum) == "primeNumber"){ // checkPrimeNum로 소수인 상황을 전제로 둠.
-                    primeNumArr.push(nums[i], nums[j], nums[k]) // 소수를 만드는 3개의 수를 담는 배열
-                    resultArr.push(primeNumArr) // 소수 만드는 배열들을 하나의 배열에 모은다.
+    let sumNumber = 0;
+    let result = 0;
+    // 숫자 세개를 골라서 합하는 코드
+    for(let i = 0; i < nums.length; i ++){
+        for(let j = i + 1; j < nums.length; j ++){
+            for(let k = j + 1; k < nums.length; k ++) {
+                sumNumber = nums[i] + nums[j] + nums[k]
+                // console.log("sumNumber", sumNumber);
+                if(checkPrimeNum(sumNumber)){ // true가 반환될때마다 return을 증가시킴
+                    result ++
                 }
             }
         }
     }
-    let result = resultArr.length;
+    // 그 숫자가 소수인지 판단하는 코드
+
+
     return result;
 }
 
-
-
-
-
-
+solution([1,2,7,6,4])
 
 
 
