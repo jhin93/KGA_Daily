a26c84eccbc45cb1e27a7b2730cd8fabb8dbfa95
jhin93 <scene1993@gmail.com>
2022-04-22 18:02:48 +0900

    settimeout을 쓰면 비동기처럼쓸수있다. 비동기 + await(동기)를 쓸 수 있다.

--- Javascript/new/async_await2.js
+++ Javascript/new/async_await2.js
@@ -1,29 +1,27 @@
-
-
-async function testAsyncAwaitFunc()
-{
-    setTimeout(() => {
-        console.log("async")
-     }, 1000);
-    
+function setTimeoutPromise(timeout) {
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+           resolve(); 
+        }, timeout);
+    })
 }
 
 
-// 함수 실행
-// testAsyncAwaitFunc();
-
+function testAsyncAwaitFunc()
+{
+    // await setTimeoutPromise(1000)
+    console.log("async")
+}
 
 
 function testFunc1() {
-    setTimeout(() => {
-        console.log('testFunc1');
-    }, 500);
+    console.log('testFunc1');
 }
 
 async function compare() {
-    await testAsyncAwaitFunc(); // 1초
-    testFunc1(); // 0.5 초
-
+    // await testAsyncAwaitFunc(); // 1초
+    testAsyncAwaitFunc(); // 1초
+    testFunc1(); 
 }
 
 // 함수 실행
@@ -31,6 +29,13 @@ compare();
 
 
 // 결과
-// testAsyncAwaitFunc 함수 내부는 await를 어떻게 하든 간에,
-// 나중에 실행한 testFunc1이 먼저 실행된다.
-// 그 이후 testAsyncAwaitFunc 에서 await에 따라 실행 순서가 바뀐다.
+// compare 함수의 상태에 따라 결과가 나눠진다.
+
+// compare 함수가 일반함수라면, 그냥 함수들이 차례대로 실행된다. 만약 함수들 중 settimeout이 걸려있다면 그게 우선적으로 적용된다.
+
+// compare 함수가 비동기함수라면(앞에 async가 붙었다면), await 유무에 따라 다르다.
+    // await가 붙은 곳은 무조건 실행하고 넘어간다. 내부에 settimeout이 몇초던 간에.
+    // await가 내부에 아무데도 없고, 먼저 실행된 함수가 비동기함수라면(async가 붙은) 일반 함수가 먼저 결과를 도출한다. 그리고 비동기 함수(async가 붙은)가 결과를 도출한다.
+
+
+    
