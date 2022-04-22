6d09b09975be48d61827b6002244eba4e4692885
jhin93 <scene1993@gmail.com>
2022-04-22 16:36:17 +0900

    await는 비동기인 async 안에서 동기적으로 작동한다.

--- Javascript/new/220422.js
+++ Javascript/new/220422.js
@@ -1,103 +1,118 @@
-// 1. promiseCheckAdult 함수를 then과 catch로 구분해서 먼저 크게 작성
-// promiseCheckAdult.then().catch()
+/*
+    비동기 처리
+    Promise
 
-// 2. 채워넣기
+    동기 처리
+*/
 
-// 밑의 async 예시를 위해 주석처리
-// function asyncCheckAdult (age) {
-//     return new Promise((resolve, reject) => {
-//         if (age >= 20) resolve(age); // resolve에 age가 들어가야 then에도 age가 들어간다
-//         else reject(age);
-//     })
-// }
+// let a = 0;
+// a();
 
-// 1번째 promise
-const promiseCheckAdult1 = asyncCheckAdult(10); // resolve를 먼저 처리해줌.
-
-promiseCheckAdult1.then((age) => { 
-    console.log(`${age} is adult!!`)
-}).catch((age) => {
-    console.log(`${age} is not adult!!`)
-});
-
-// 2번째 promise
-const promiseCheckAdult2 = asyncCheckAdult(21);
-
-promiseCheckAdult2.then((age) => { 
-    console.log(`${age} is adult!!`)
-}).catch((age) => {
-    console.log(`${age} is not adult!!`)
-});
-
-
-
-
-// 비동기는 실행시켜놓고 다른 일을 할 때 쓴다.
-
-
-
+function setTimeoutPromise(timeout) {
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+           resolve(); 
+        }, timeout);
+    })
+}
 
 // async await
-// async는 프로미스 함수를 간략하게 만듬
+async function timeoutCheckAdult(age, timeout) {
+    
+    console.log(`${age}. timeoutCheckAdult`);
+    await setTimeoutPromise(timeout);
+    console.log(`${age}. timeoutCheckAdult`);
 
-async function asyncCheckAdult (age) {
-    if (age >= 20) return age;
+    if (age > 20) return true;
+    return false;
+}
+
+async function asyncCheckAdult(age) {
+    if (age >= 20)  { return age; }
     else throw new Error(age);
 }
 
+// await 키워드 사용 함수의 종료를 기다리지 않고 다음 함수를 호출한다.
+
+// function asyncCheckAdult(age) {
+//     return new Promise((resolve, reject) => {
+//         if (age >= 20)  resolve(age);
+//         else    reject(age);
+//     })
+// }
+
+async function testAsyncAwaitFunc()
+{
+    // timeoutCheckAdult(10, 8000);
+    // timeoutCheckAdult(20, 5000);
+    // timeoutCheckAdult(30, 1000);
+
+    await timeoutCheckAdult(10, 8000); // 이 문장이 끝날때까지 아랫문장을 실행하지 않는다. wait 하는 것. 위의 사례와는 다르다.
+    await timeoutCheckAdult(20, 5000);
+    await timeoutCheckAdult(30, 1000);
 
 
+    // let promises = [];
+
+    // promises.push(timeoutCheckAdult(10, 8000));
+    // promises.push(timeoutCheckAdult(20, 5000));
+    // promises.push(timeoutCheckAdult(30, 1000));
+
+    // let results = await Promise.all(promises);
+    // console.log(results);
 
 
+    // const promiseCheckAdult = asyncCheckAdult(10);
+    // promiseCheckAdult.then((age) => {
+    //     console.log(`${age} is adult!!`);
+    // }).catch((age) => {
+    //     console.log(`${age} is not adult!!`);
+    // });
+    
+    // const promiseCheckAdult1 = asyncCheckAdult(21);
+    // promiseCheckAdult1.then((age) => {
+    //     console.log(`${age} is adult!!`);
+    // }).catch((age) => {
+    //     console.log(`${age} is not adult!!`);
+    // });
+}
 
+testAsyncAwaitFunc();
 
 
 
 // const promise = new Promise(() => {
-
+     
 // });
 
 /*
     new Promise 호출과 동시에 비동기 처리 시작
 */
-const promise = new Promise((resolve, reject) => { // new 는 런타임 중에 메모리가 할당되기 때문에, 아래 함수들 먼저 실행된다. 아래 함수들은 컴파일될때 이미 실행
-    /*
-        시간이 오래 걸리는 실행문 ... 5초
-    */
-    resolve(); // then과 매칭
-    reject(); // catch랑 매칭
-});
+//  const promise = new Promise((resolve, reject) => {
+//     /*
+//         시간이 오래 걸리는 실행문 ... 5초
+//     */
+//     reject();
+//     resolve();
+//  });
 
+//  promise.then(() => {
+//             console.log('1. promsie() then() called');
+//      }).catch(() => {
+//            console.log('2. promsie() catch() called');
+//     });
 
-promise.then(() => {
-    console.log('1. promise() then()  called');
-}).catch(() => {
-    console.log('2. promise() catch()  called');
-})
+// function testFunc1() {
+//     console.log('testFunc1()');
 
+//     let startTime = new Date().getTime();
+//     while(new Date().getTime() - startTime < 100);
 
-// let a = 0;
-// a();
+//     testFunc2();
+// }
 
-function testFunc1() {
-    console.log('testFunc1()')
+// function testFunc2() {
+//     console.log('testFunc2()');
+// }
 
-    let startTime = new Date().getTime();
-    while(new Date().getTime() - startTime < 1000);
-
-    testFunc2();
-}
-
-function testFunc2() {
-    console.log('testFunc2()')
-}
-
-
-// testFunc1 - 함수를 호출할 수 있는 포인터
-testFunc1();
-
-
-
-
-
-
+// testFunc1();
