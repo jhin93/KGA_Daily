55f23b0bc59861d16b29326ee2202486b84000ad
jhin93 <scene1993@gmail.com>
2022-04-28 11:13:05 +0900

    암호화 모듈 설치 + 사용

--- Javascript/new/blockchain/block.js
+++ Javascript/new/blockchain/block.js
@@ -7,6 +7,7 @@
     hash : 블록 내부 데이터로 생성한 sha256 값 (블록의 유일성)
     previous hash : 이전 블록의 해쉬 (이전 블록을 참조)
 */
+import CryptoJS from 'crypto-js'
 
 class Block {
     constructor (index, data, timestamp, hash, previoushash) {
@@ -22,6 +23,12 @@ const blocks = [1, 2, 3, 4, 5];
 
 function getBlocks () {
     return blocks;
+}
+
+// 해쉬값 계산
+const calculateHash = (index, data, timestamp, previoushash) => {
+    // 암호화 모듈 사용
+    CryptoJS.SHA256(index + data + timestamp + previoushash);
 }
 
 export { getBlocks }
