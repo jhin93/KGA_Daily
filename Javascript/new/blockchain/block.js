d02b38443071a85c81fc6300be9c7ab1f8a1bee4
jhin93 <scene1993@gmail.com>
2022-04-28 11:40:00 +0900

    블록 생성 함수

--- Javascript/new/blockchain/block.js
+++ Javascript/new/blockchain/block.js
@@ -19,7 +19,7 @@ class Block {
     }
 }
 
-const blocks = [1, 2, 3, 4, 5];
+const blocks = [];
 
 function getBlocks () {
     return blocks;
@@ -30,10 +30,35 @@ const calculateHash = (index, data, timestamp, previoushash) => {
     // 암호화 모듈 사용
     return CryptoJS.SHA256((index + data + timestamp + previoushash).toString()).toString(); // toString을 붙이면 16진수 64자리 해쉬 형태로 나온다. 해당 블록이 유일무이하다는 증거.
     // sha256은 string을 받기 때문에 안쪽에도 toString을 사용
-    // 아래 테스트에서 매개변수 int값이 조금만 변해도 해쉬값이 판이하게 변함
+    // int값이 조금만 변해도 해쉬값이 판이하게 변함
 }
 
-let test = calculateHash(11, 20, 30, 40);
-console.log('test : ', test);
+
+// genesis 블록 생성 함수
+const createGenesisBlock = () => {
+    const genesisBlock = new Block(0, 'the times 03/jan/2009 chancellor on brink of second bailout for banks', 0, 0, 0);
+    genesisBlock.hash = calculateHash(genesisBlock.index, genesisBlock.data, genesisBlock.timestamp, genesisBlock.previoushash);
+}
+
+
+// 일반 블록 생성 함수
+const createBlock = (blockData) => {
+    const previousBlock = blocks[blocks.length - 1]; // 가장 마지막 블록
+    const nextIndex = previousBlock.index + 1;
+    const nextTimestamp = new Date().getTime() / 1000 //밀리세컨드 단위를 초단위로 만듬
+    const nextHash = calculateHash(nextIndex, blockData, nextTimestamp, previousBlock.hash) // 이전 블록의 정보들을 가지고 다음 블록 해쉬 구하기
+    
+    const newBlock = new Block(nextIndex, blockData, nextTimestamp, nextHash, previousBlock.hash);
+
+    return newBlock;
+}
+
 
 export { getBlocks }
+
+
+
+
+
+
+
