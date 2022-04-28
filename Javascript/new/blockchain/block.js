850cee272e8723ca5629c9007981ca40547fbfde
jhin93 <scene1993@gmail.com>
2022-04-28 10:17:58 +0900

    block.js의 데이터를 import해서 조회하기

--- Javascript/new/blockchain/block.js
+++ Javascript/new/blockchain/block.js
@@ -1,4 +1,10 @@
 // 블록체인 관련 함수
 // 블록 구조 설계
 
-const blocks = [];
+const blocks = [1, 2, 3, 4, 5];
+
+function getBlocks () {
+    return blocks;
+}
+
+export { getBlocks }
--- Javascript/new/blockchain/httpServer.js
+++ Javascript/new/blockchain/httpServer.js
@@ -1,6 +1,7 @@
 // 웹에 명령어를 입력해서 내 노드를 제어하는 서버
 import express from 'express';
 import bodyParser from 'body-parser';
+import { getBlocks } from './block.js';
 
 
 // 초기화 함수
