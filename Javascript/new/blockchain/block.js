226dd1948ee719d8d80ed5023940ab194bf3b5ee
jhin93 <scene1993@gmail.com>
2022-04-28 10:48:40 +0900

    블록 클래스 작성

--- Javascript/new/blockchain/block.js
+++ Javascript/new/blockchain/block.js
@@ -1,5 +1,22 @@
 // 블록체인 관련 함수
 // 블록 구조 설계
+/*
+    index : 블록체인의 높이
+    data : 블록에 포함된 모든 데이터 (트랜잭션 포함)
+    timestamp : 블록이 생성된 시간
+    hash : 블록 내부 데이터로 생성한 sha256 값 (블록의 유일성)
+    previous hash : 이전 블록의 해쉬 (이전 블록을 참조)
+*/
+
+class Block {
+    constructor (index, data, timestamp, hash, previoushash) {
+        this.index = index;
+        this.data = data;
+        this.timestamp = timestamp;
+        this.hash = hash;
+        this.previoushash = previoushash;
+    }
+}
 
 const blocks = [1, 2, 3, 4, 5];
 
