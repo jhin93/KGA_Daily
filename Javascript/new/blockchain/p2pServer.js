b1a6f0f1b3ea14f991a50b4b57e404fcd6e4c34e
jhin93 <scene1993@gmail.com>
2022-04-29 12:06:27 +0900

    getPeers 추가

--- Javascript/new/blockchain/p2pServer.js
+++ Javascript/new/blockchain/p2pServer.js
@@ -4,6 +4,10 @@ import { WebSocketServer } from 'ws';
 
 const sockets = [];
 
+const getPeers = () => {
+    return sockets;
+}
+
 const initP2PServer = (p2pPort) => {
     const server = new WebSocketServer({port:p2pPort}); // port를 매개변수로 받겠다. WebSocketServer는 포트를 넣어주면 웹소켓 서버를 만들어주는 메소드
     server.on('connection', (ws) => { // connection은 웹소켓으로 만든 서버에 연결해주는 이벤트. 커넥션이 일어나면 , 다음에 따라오는 함수를 실행한다
@@ -19,8 +23,8 @@ const initConnection = (ws) => {
 
 const connectionToPeer = (newPeer) => {
     const ws = new WebSocket(newPeer)
-    ws.on('open', () => { initConnection(ws); })
-    ws.on('error', () => { console.log('Fail to Connection peer : ', ws.remoteAddress); return false})
+    ws.on('open', () => { initConnection(ws); console.log('Connection peer : ', newPeer); })
+    ws.on('error', () => { console.log('Fail to Connection peer : ', newPeer); })
 }
 
 export { initP2PServer, connectionToPeer };
