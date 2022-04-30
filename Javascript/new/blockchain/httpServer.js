d1bd2a08b604090acfc57d5d92379ea90a8e9f90
jhin93 <scene1993@gmail.com>
2022-04-30 18:00:34 +0900

    민수랑 만든거로 교체

--- Javascript/new/blockchain/httpServer.js
+++ Javascript/new/blockchain/httpServer.js
@@ -1,59 +1,74 @@
 // 웹에 명령어를 입력해서 내 노드를 제어하는 서버
-import express from 'express';
+import express from 'express'; //속도나 크기면에서 require에 비해 compact함
+// const express = require('express'); 
 import bodyParser from 'body-parser';
 import { getBlocks, createBlock } from './block.js';
-import { connectionToPeer, getPeers, sendMessage } from './p2pServer.js';
+import { connectToPeer, getPeers, SendMessage } from './p2pServer.js'
+import nunjucks from 'nunjucks';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // 초기화 함수
 const initHttpServer = (myHttpPort) => {
     const app = express();
+    app.use(express.static(__dirname + "/public"))
+    app.set('view engine', 'html');
+    nunjucks.configure('views', {
+        express: app,
+    })
+
     app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
 
     app.get('/', (req, res) => {
-        res.send("Hello, World")
+
+        res.render('p2pclient')
     })
 
-
-    //block.js의 블록 데이터를 조회
-    app.get('/getblocks', (req, res) => {
-        res.send(getBlocks()); // getBLock 함수를 실행시킨 것을 import 해온다.
-        console.log("req.body : ", req.body)
+    app.get('/blocks', (req, res) => {
+        res.send(getBlocks());
     })
 
-    app.post('/createBlock', (req, res) => {
-        res.send(createBlock(req.body.data)) // postman에서 입력한 값을 여기서 전달해준다.
-        console.log("req.body.data : ", req.body)
-    })
+    app.post('/blocks', (req, res) => {
 
+        res.send(createBlock(req.body.data));
+
+    })
     app.post('/addPeer', (req, res) => {
-        console.log("send : ", req.body.send)
-        res.send(connectionToPeer(req.body.data)) 
-        // 포스트맨에서 다음과 같이 작성해서 보내면 뜬다.
-        // {
-        //     "data" : "ws://192.168.0.211:6001", // 받는 사람 ip 입력하고 받는 사람이 6001을 열어놔야 가능
-        //     "message" : "test" // 이건 내 콘솔에서 뜸
-        // }
+
+        const { ipAddress, port } = req.body
+
+        let fullAddress = "ws://" + ipAddress + ":" + port;
+        console.log(fullAddress)
+        connectToPeer(fullAddress);
+        console.log("connectToPeer")
+
     })
-    
+    app.get('/addPeer', (req, res) => {
+        res.redirect('/')
+
+    })
+    app.get('/peers', (req, res) => {
+        res.send(getPeers());
+
+    })
     app.post('/sendMessage', (req, res) => {
-        sendMessage(req.body.data);
+        
+        let data= {
+
+            "message" : req.body.msg,
+            "type" : parseInt(req.body.type)
+        }
+        console.log(data)
+        SendMessage(data);
+
     })
-
-
-
-
-
-    app.listen(myHttpPort, () => {// listen은 매개변수가 2개.
+    app.listen(myHttpPort, () => {
         console.log('listening httpServer Port : ', myHttpPort);
-    })
-
-}
-
-export {initHttpServer}
-
-
-
-
-
-
+    });
+};
+export { initHttpServer }
